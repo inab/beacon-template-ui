72c8961956e86d2e1b9ff1535af83612bbcdd621
Juancris Pérez <juancrisantos.perez@gmail.com>
2026-06-23 14:25:31 +0200

    re-enable central filtering-terms search input in networkBeacon mode now that it correctly queries the aggregator

--- src/components/Search.js
+++ src/components/Search.js
@@ -230,10 +230,6 @@ export default function Search({
   };
 
   const renderInput = (type) => {
-    if (type !== "genomic" && CONFIG.beaconType === "networkBeacon") {
-      return null;
-    }
-
     if (type === "genomic") {
       return (
         <Box
